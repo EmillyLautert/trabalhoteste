86116fb91dc8a767552d7fc46bc604a589f8a7ff
EmillyLautert <emilly.santos@atitus.edu.br>
2026-03-16 20:19:38 -0300

    Rename attributes in Cliente class

--- cliente.py
+++ cliente.py
@@ -1,10 +1,10 @@
 class Cliente:
     def __init__(self, id, nome):
-        self.id = id
-        self.nome = nome
+        self.idCliente = idCliente
+        self.nomeCliente = nomeCliente
 
     def to_csv(self):
-        return f"{self.id};{self.nome}"
+        return f"{self.idCliente};{self.nomeCliente}"
 
     def __str__(self):
-        return f"ID: {self.id} | Nome: {self.nome}"
+        return f"ID: {self.idCliente} | Nome: {self.nomeCliente}"
