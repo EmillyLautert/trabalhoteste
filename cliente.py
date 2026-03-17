8e465e310345dda9c049488ef04f9a10543aa43a
EmillyLautert <emilly.santos@atitus.edu.br>
2026-03-17 10:43:44 -0300

    Ajustado conforme vf

--- cliente.py
+++ cliente.py
@@ -1,10 +1,12 @@
 class Cliente:
+    """Representa um cliente cadastrado no sistema."""
+
     def __init__(self, idCliente, nomeCliente):
         self.idCliente = idCliente
         self.nomeCliente = nomeCliente
 
-    def to_csv(self):
-        return f"{self.idCliente};{self.nomeCliente}"
-
     def __str__(self):
-        return f"ID: {self.idCliente} | Nome: {self.nomeCliente}"
+        return f"ID: {self.idCliente}, Nome: {self.nomeCliente}"
+
+    def to_csv(self):
+        return f"{self.idCliente},{self.nomeCliente}\n"
