24976706b88d481239f03931a1305fc69f746c20
EmillyLautert <emilly.santos@atitus.edu.br>
2026-03-17 10:47:30 -0300

    Ajustado para vf

--- fila.py
+++ fila.py
@@ -1,25 +1,27 @@
+''' GABI'''
+
 class Fila:
     def __init__(self):
-        self._itens = []
+        self.itens = []
 
     def enqueue(self, item):
-        self._itens.append(item)
+        self.itens.append(item)
 
     def dequeue(self):
-        if self.esta_vazia():
+        if self.estaVazia():
             return None
-        return self._itens.pop(0)
+        return self.itens.pop(0)
 
     def front(self):
         if self.estaVazia():
             return None
-        return self._itens[0]
-
-    def estaVazia(self):
-        return len(self._itens) == 0
+        return self.itens[0]
 
     def listar(self):
-        return self._itens
+        return self.itens
+
+    def estaVazia(self):
+        return len(self.itens) == 0
 
     def tamanho(self):
-        return len(self._itens)
+        return len(self.itens)
