e95720229a96b5949b5706469f579f0eae0f0d1c
EmillyLautert <emilly.santos@atitus.edu.br>
2026-03-17 11:09:46 -0300

    Ajustado para vf

--- main.py
+++ main.py
@@ -1,70 +1,58 @@
-'''MATHIAS AQUI'''
+'''MATHIAS'''
 
 from estoque import SistemaEstoque
 
-def menu():
-    print("\n===== MENU ESTOQUE =====")
+
+def exibirMenu():
+    print("\n===== MENU DO SISTEMA =====")
     print("1 - Cadastrar cliente")
     print("2 - Listar clientes")
     print("3 - Cadastrar produto")
     print("4 - Listar produtos")
     print("5 - Pesquisar produto")
     print("6 - Realizar venda")
-    print("7 - Ver fila de vendas")
+    print("7 - Visualizar fila de vendas")
     print("8 - Desfazer última operação")
     print("9 - Exibir valor total do estoque")
     print("10 - Exibir valor total de vendas")
     print("11 - Exibir clientes e valores gastos")
     print("12 - Sair")
-    print("========================")
 
 
 def main():
-    estoque = SistemaEstoque()
+    sistema = SistemaEstoque()
 
     while True:
-        menu()
+        exibirMenu()
         opcao = input("Escolha uma opção: ").strip()
 
         if opcao == "1":
-            estoque.cadastrarCliente()
-
+            sistema.cadastrarCliente()
         elif opcao == "2":
-            estoque.listarClientes()
-
+            sistema.listarClientes()
         elif opcao == "3":
-            estoque.cadastrarProduto()
-
+            sistema.cadastrarProduto()
         elif opcao == "4":
-            estoque.listarProdutos()
-
+            sistema.listarProdutos()
         elif opcao == "5":
-            estoque.pesquisarProduto()
-
+            sistema.pesquisarProduto()
         elif opcao == "6":
-            estoque.realizarVenda()
-
+            sistema.realizarVenda()
         elif opcao == "7":
-            estoque.visualizarFilaVendas()
-
+            sistema.visualizarFilaVendas()
         elif opcao == "8":
-            estoque.desfazerUltimaOperacao()
-
+            sistema.desfazerUltimaOperacao()
         elif opcao == "9":
-            estoque.valorTotalEstoque()
-
+            sistema.exibirValorTotalEstoque()
         elif opcao == "10":
-            estoque.valorTotalVendas()
-
+            sistema.exibirValorTotalVendas()
         elif opcao == "11":
-            estoque.clientesValoresGastos()
-
+            sistema.exibirClientesEValoresGastos()
         elif opcao == "12":
-            print("Encerrando o sistema...")
+            print("Sistema encerrado.")
             break
-
         else:
-            print("Opção inválida. Tente novamente.")
+            print("Opção inválida.")
 
 
 main()
