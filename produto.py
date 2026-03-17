cdddcf27d8b4c2b546f40d8212e06e356e569b1d
EmillyLautert <emilly.santos@atitus.edu.br>
2026-03-17 10:44:11 -0300

    Ajustado conforme vf

--- produto.py
+++ produto.py
@@ -1,12 +1,14 @@
 class Produto:
-    def __init__(self, idProduto, nomeProduto, quantidadeProduto, precoProduto):
+    """Representa um produto disponível no estoque."""
+
+    def __init__(self, idProduto, nomeProduto, quantidadeEstoque, precoProduto):
         self.idProduto = idProduto
         self.nomeProduto = nomeProduto
-        self.quantidadeProduto = quantidadeProduto
+        self.quantidadeEstoque = quantidadeEstoque
         self.precoProduto = precoProduto
 
-    def to_csv(self):
-        return f"{self.idProduto};{self.nomeProduto};{self.quantidadeProduto};{self.precoProduto}"
-
     def __str__(self):
-        return f"ID: {self.idProduto} | Nome: {self.nomeProduto} | Qtd: {self.quantidadeProduto} | Preço: R$ {self.precoProduto:.2f}"
+        return f"ID: {self.idProduto}, Nome: {self.nomeProduto}, Quantidade: {self.quantidadeEstoque}, Preço: R${self.precoProduto:.2f}"
+
+    def to_csv(self):
+        return f"{self.idProduto},{self.nomeProduto},{self.quantidadeEstoque},{self.precoProduto}\n"
